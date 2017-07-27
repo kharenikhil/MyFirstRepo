b21e2ecdae90419e01680b0c47d056f9fccce15d
kharenikhil <nkhare@microsoft.com>
2017-07-27 11:41:23 -0700

    Add project files.

---
+++ MyFirstRepo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstRepo.Startup))]
+namespace MyFirstRepo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
